bcad2fb3a5012d18d7439fd8c9f9c5e42471a854
Steven Torrence <storrence88@gmail.com>
2019-10-09 21:59:25 -0500

    Add main controller and index action as well as bootstrap navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'main/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'main#index'
 end
