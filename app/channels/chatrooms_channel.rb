e6d12120b487330fb6b548aef4b1b6b468b7f774
Steven Torrence <storrence88@gmail.com>
2019-10-19 21:34:36 -0500

    Set up the current user's chatrooms and the ability to leave a channel

--- app/channels/chatrooms_channel.rb
+++ app/channels/chatrooms_channel.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 class ChatroomsChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    current_user.chatrooms.each do |chatroom|
+      stream_from "chatroom: #{chatroom.id}"
+    end
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
   end
 end
